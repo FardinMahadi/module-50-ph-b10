d7013f7b7e879fd67d086a7a04257b54e45d7bbe
FardinMahadi <mahadihasanfardin2015@gmail.com>
2024-12-09 22:19:53 +0600

    done

---
+++ src/Components/SignUp/SignUp.jsx
@@ -0,0 +1,42 @@
+const SignUp = () => {
+  return (
+    <div className="card w-full max-w-sm shrink-0 shadow-2xl text-center mx-auto mt-10">
+      <h3 className="text-3xl font-bold">Sign Up now!</h3>
+
+      <form className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input
+            type="email"
+            placeholder="email"
+            className="input input-bordered"
+            required
+          />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input
+            type="password"
+            placeholder="password"
+            className="input input-bordered"
+            required
+          />
+          <label className="label">
+            <a href="#" className="label-text-alt link link-hover">
+              Forgot password?
+            </a>
+          </label>
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Sign Up</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
